1af4e8c34808570819f3caa6b8c468f2c3c23e57
Jose Sicajan <jlsicajan@gmail.com>
2019-11-05 01:51:22 -0600

    I am Batman, Center widget and center text

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 
 // The main function is the starting point for all our Flutter apps.
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('I am Rich'),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Center(child: Text('I Am Batman')),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://i0.wp.com/fullcirclecinema.com/wp-content/uploads/2019/09/Untitled-1.png?resize=960%2C600&ssl=1'),
         ),
       ),
-    )
-  );
+    ),
+  ));
 }
